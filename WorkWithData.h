006da4410a29d7b904d52d68f35c1e519c990857
Stepan <76563428+Mr-Grag-Universe@users.noreply.github.com>
2022-02-07 15:33:42 +0300

    Add files via upload

--- WorkWithData.h
+++ WorkWithData.h
@@ -3,4 +3,5 @@
 void insert(Matrix ** MATRIX, size_t * nom_);
 void delete(Matrix ** MATRIX, size_t * nom_);
 void all_minors(Matrix * MATRIX, size_t nom);
+void find_rang(Matrix * MATRIX, size_t nom);
 #endif
